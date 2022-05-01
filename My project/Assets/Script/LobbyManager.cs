8aef51bfc0fd0c6b782f7c74d8135262a2a557e6
mukuxie <57396715+mukuxie@users.noreply.github.com>
2022-05-01 12:05:06 +0800

    20200501_連線功能製作_02

--- My project/Assets/Script/LobbyManager.cs
+++ My project/Assets/Script/LobbyManager.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
 
     [SerializeField, Header("連線中畫面")]
     private GameObject goConnectView;
+    [SerializeField, Header("對戰按鈕")]
+    private Button btnBattle;
+    [SerializeField, Header("連線人數")]
+    private Text textCountPlayer;
 
     private void Awake()
     {
@@ -15,15 +21,48 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster() // override 允許複寫的副類別
     {
         base.OnConnectedToMaster(); // 連線至控制台
-        print("以連線至控制台");
+        print("<color=yellow>已經進入控制台</color>");
+
+        PhotonNetwork.JoinLobby();
     }
 
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        print("<color=yellow>已經進入大廳</color>");
+
+        btnBattle.interactable = true;
+    }
 
 
     public void StartConnect()
     {
-        print("開始連線...");
+        print("<color=yellow> 開始連線</color>");
 
         goConnectView.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
     }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        print("<color=red> 加入隨機房間失敗</color>");
+        RoomOptions ro = new RoomOptions();
+        ro.MaxPlayers = 5;
+        PhotonNetwork.CreateRoom("", ro);
+        
+    }
+
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+        print("<color=yellow> 開房者進入房間</color>");
+        int currentCount = PhotonNetwork.CurrentRoom.PlayerCount;
+        int maxCount = PhotonNetwork.CurrentRoom.MaxPlayers;
+
+        textCountPlayer.text = "連線人數" + currentCount + " / " + maxCount;
+    }
+
+
+
 }
